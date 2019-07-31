3a10447145298d59f96ae19730be447a150d4af4
Hasan Mahmud <hasanmahmud548589@gmail.com>
2019-07-31 23:00:12 +0600

    React JSX

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Test from "./Components/Test";
+
+function Main() {
+  return (
+    <div>
+        <Test />
+    </div>
+  );
+}
+export default Main;
---
+++ src/Components/Test.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+
+export default class Test extends Component {
+    render() {
+    // const name = "Hasan Mahmud";
+    // const Person = <h1>Person name : {name}!</h1>
+
+    // function nameFprmat(user) {
+    //     return user.fastName + ' ' + user.lastName;
+    // }
+    // const user = {
+    //     fastName: 'Yousuf',
+    //     lastName: 'Ali'
+    // }
+    // const Element = (
+    //     <h2>Hello, {nameFprmat(user)}! </h2>
+    // )
+    // function getGreeting(user) {
+    // if (user) {
+    //     return <h1>Hello, {formatName(user)}!</h1>;
+    // }
+    // return <h1>Hello, Stranger.</h1>;
+    // }
+
+
+        return user
+    }
+}
